4aaba5e3323b55569b1ceee45e1ba63a0fec13e9
adityamahendrap <ptadityamahendrap@gmail.com>
2023-12-06 22:23:21 +0800

    add: resident model

--- app/Models/User.php
+++ app/Models/User.php
@@ -74,4 +74,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Presence::class);
     }
+
+    public function residents(): HasOne
+    {
+        return $this->hasOne(Resident::class);
+    }
 }
