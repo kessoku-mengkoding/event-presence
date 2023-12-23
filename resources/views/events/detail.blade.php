9391f694ec97e6bd9a45abf0ede76e39629975a0
adityamahendrap <ptadityamahendrap@gmail.com>
2023-12-23 18:50:29 +0800

    add: detail event

--- resources/views/events/detail.blade.php
+++ resources/views/events/detail.blade.php
@@ -1,83 +1,79 @@
 @extends('layouts.main')
 
 @section('content')
-  <h1> Event name: {{ $event->name }} </h1>
-  <p>Event Id: {{ $event->id }}</p>
-  <p>Event Description: {{ $event->description }}</p>
-  <p>Qr: {{ $event->qr_code_path }}</p>
-  <img src="{{ $event->qr_code_path }}" alt="">
-  <p class="mt-4">Event Members:</p>
-  <ul>
-    @foreach ($event->eventmembers as $eventmember)
-      <li class="flex gap-12 border border-black">
-        <p>{{ $eventmember->user->username }}</p>
-        <p>{{ $eventmember->user->name }}</p>
-        <p>{{ $eventmember->user->email }}</p>
-        <p>{{ $eventmember->role }}</p>
-        <form action="/events/{{ $event->id }}/member/{{ $eventmember->user->id }}" method="POST">
-          @csrf
-          @method('DELETE')
-          <button class="btn">Delete</button>
-        </form>
-        <form action="/events/{{ $event->id }}/members/{{ $eventmember->id }}" method="POST">
-          @csrf
-          @method('DELETE')
-          <button class="btn">Delete</button>
-        </form>
-      </li>
-    @endforeach
-  </ul>
+  <a href="/events"
+    class="mb-4 inline-block rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">Kembali</a>
+  <h1 class="text-3xl font-bold">Kegiatan {{ $event->name }} </h1>
+  <p class="mt-4">{{ $event->description }}</p>
+  <p class="mt-4">Qr: {{ $event->qr_code_path }}</p>
+  <img src="{{ $event->qr_code_path }}" alt="Join by QR Code">
 
-  <br>
-  <br>
-  <br>
-
-  <h1>Invite member</h1>
-  <div class="flex">
-    <p>invite by email or username</p>
-    <form action="/events/{{ $event->id }}/invite" method="POST">
-      @csrf
-      @method('POST')
-      <input type="text" name="key" id="" class="input">
-    </form>
+  <div class="mt-8">
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div class="flex justify-between justify-items-center bg-white pb-4 dark:bg-gray-900">
+        <label for="table-search" class="sr-only">Search</label>
+        <div class="relative mt-1">
+          <div class="rtl:inset-r-0 pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
+            <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            </svg>
+          </div>
+          <input type="text" id="table-search"
+            class="block w-80 rounded-lg border border-gray-300 bg-gray-50 ps-10 pt-2 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            placeholder="Search for items">
+        </div>
+        <div>
+          <button
+            class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto">Tambah
+            Anggota</button>
+        </div>
+      </div>
+      <div class="mt-2">Total Anggota: {{ $event->eventmembers->count() }}</div>
+      <table class="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
+        <thead class="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" class="p-4">
+              No
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Nama Lengkap
+            </th>
+            <th scope="col" class="px-6 py-3">
+              NIK
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Email
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($event->eventmembers as $eventmember)
+            <tr class="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
+              <td class="w-4 p-4">
+                {{ $loop->iteration }}
+              </td>
+              <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+                {{ optional($eventmember->user->resident)->full_name }}
+              </th>
+              <td class="px-6 py-4">
+                {{ optional($eventmember->user->resident)->nik }}
+              </td>
+              <td class="px-6 py-4">
+                {{ $eventmember->user->email }}
+              </td>
+              <td class="flex items-center px-6 py-4">
+                {{-- <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Edit</a> --}}
+                <a href="#" class="ms-3 font-medium text-red-600 hover:underline dark:text-red-500">Remove</a>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
   </div>
-
-  <br>
-  <br>
-  <br>
-
-  <form action="/events/{{ $event->id }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <button class="btn">Delete Event</button>
-  </form>
-
-  <br>
-  <br>
-  <br>
-
-  <a href="/timetables/{{ $event->id }}/new" class="btn">Create Timetable</a>
-  <h1>Timetables</h1>
-  <ul>
-    @foreach ($timetables as $timetable)
-      <li class="flex gap-12 border border-black">
-        <p>{{ $timetable->title }}</p>
-        <p>{{ $timetable->start }}</p>
-        <p>{{ $timetable->end }}</p>
-        <p>{{ $timetable->lateness_tolerance }}</p>
-        <p>{{ $timetable->lat }}</p>
-        <p>{{ $timetable->long }}</p>
-        <p>{{ $timetable->radius_meter }}</p>
-        <p>{{ $timetable->address }}</p>
-        <a href="/timetables/{{ $timetable->id }}/presences" class="btn">Detail</a>
-        <form action="/timetables/{{ $timetable->id }}" method="POST">
-          @csrf
-          @method('DELETE')
-          <button class="btn">Delete</button>
-        </form>
-      </li>
-      <img src="{{ $timetable->qr_code_path }}" alt="">
-      {{ $timetable->qr_code_path }}
-    @endforeach
-  </ul>
 @endsection
